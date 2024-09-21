1d96f192ed3958d6f06c1f337cfa0914c861ebe8
ayushqui <proayush736@gmail.com>
2024-09-21 15:27:21 +0000

    adapt to lmodroid

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_everpal.mk
+    $(LOCAL_DIR)/lmodroid_everpal.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_everpal-user \
-    lineage_everpal-userdebug \
-    lineage_everpal-eng
+    lmodroid_everpal-user \
+    lmodroid_everpal-userdebug \
+    lmodroid_everpal-eng
